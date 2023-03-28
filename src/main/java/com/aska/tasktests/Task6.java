57494f3828998c40c5c8b9bca915c02326a3a723
aVoz <caya2321@gmail.com>
2023-03-28 13:18:33 +0200

    Task 6 update

--- src/main/java/com/aska/tasktests/Task6.java
+++ src/main/java/com/aska/tasktests/Task6.java
@@ -1,16 +1,26 @@
 package com.aska.tasktests;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task6 {
 
     public static Object task6Expres(List<Integer> wordList1, List<String> wordList2) {
-        var k =wordList2
+
+    return  wordList1
                 .stream()
-                .filter(str -> Character.isDigit(str.charAt(0)))
+                .filter(Objects::nonNull)
+                .map(intStr -> wordList2
+                        .stream()
+                        .filter(str2 -> str2.length() > 0
+                                & str2.length() == intStr
+                                & Character.isDigit(str2.charAt(0)))
+                        .findFirst())
+                .flatMap(o -> o.isPresent() ? Stream.of(o.get()) : Stream.of("Not Found"))
                 .collect(Collectors.toList());
-        return  k;
     }
 }
 
